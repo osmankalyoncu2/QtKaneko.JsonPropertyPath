72d7ef492f5919c6e9c3022998b1cf8ad4271159
Kaneko <mail.qt.kaneko@gmail.com>
2022-03-20 09:33:19 +0200

    Add copyright notices

--- Helpers/JsonConverterExtensions.cs
+++ Helpers/JsonConverterExtensions.cs
@@ -5,6 +5,7 @@
 // QtKaneko.JsonPropertyPath is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License along with QtKaneko.JsonPropertyPath. If not, see <https://www.gnu.org/licenses/>.
+// Copyright 2022 Kaneko Qt
 
 using System.Linq.Expressions;
 using System.Reflection;
